af43fea12fb4a3b99fdc3d81749d4246cabd7204
d_hristoskov <dimitar.hristoskov@valtech.com>
2021-09-01 23:03:03 +0200

    minor style changes and add form

--- components/dashboard/kid-dashboard/dashboard-sections/donate-items/donate-form.tsx
+++ components/dashboard/kid-dashboard/dashboard-sections/donate-items/donate-form.tsx
@@ -1,9 +1,52 @@
 import { NextPage } from "next"
+import { ChangeEvent, FormEvent, useState } from "react";
 
+
+//WIP
 const DonateForm: NextPage = (): JSX.Element => { 
 
+    const [ registerItem, setRegisterItem ] = useState({
+        item: '',
+        type: '',
+        condition: ''
+     });
+
+     const { item, type, condition } = registerItem;
+
+     const onHandleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target;
+        setRegisterItem({...registerItem, [name]: value})
+    };
+
+    const onHandleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault(); 
+        console.log(registerItem);
+        setRegisterItem({
+            item: '',
+            type: '',
+            condition: ''
+        });
+    };
+
+     
     return (
-        <div>Form</div>
+        <div>
+            <form onSubmit={onHandleSubmit}>
+                <div>
+                    <input type='text' name='item' placeholder='Describe your Item'  
+                    value={item} onChange={onHandleChange} />
+                </div>
+                <div>
+                    <input type="text" name='type' placeholder='Type of item'
+                    value={type} onChange={onHandleChange}/>
+                </div>
+                <div>
+                    <input type="text" name='condition' placeholder='Condition'
+                    value={condition} onChange={onHandleChange}/>
+                </div>
+                <input type='submit' value='Submit' />
+            </form>
+        </div>
     )
 }
 
--- components/dashboard/kid-dashboard/dashboard-sections/donate-items/donate-items.tsx
+++ components/dashboard/kid-dashboard/dashboard-sections/donate-items/donate-items.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from "next";
 
 import ColourSection from "@/components/shared-components/sections/colour-section";
+import DonateForm from "./donate-form";
 
 const DonateItems: NextPage = (): JSX.Element => {
     return (
         <ColourSection bgColour='#66B8EC'>
-            <p>Donate</p>
+            <DonateForm />
         </ColourSection>
     )
 }
