4367d8d2ec08d0bd3766dfac5d1cc0be99dfbc1d
Jamie Smith <jeasmith@users.noreply.github.com>
2021-09-14 22:41:26 +0100

    Accept page

--- components/shared-components/input-fields/text-input-field.tsx
+++ components/shared-components/input-fields/text-input-field.tsx
@@ -1,32 +1,41 @@
-import { Input } from "@chakra-ui/react"
-import { NextPage } from "next"
-import { ChangeEvent } from "react"
+import { Input } from "@chakra-ui/react";
+import { NextPage } from "next";
+import { ChangeEvent } from "react";
 
 interface Props {
-    name?: string;
-    placeholder?: string;
-    isPassword: boolean;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
+  placeholder?: string;
+  isPassword: boolean;
+  value?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  white?: boolean;
 }
 
-const TextInputField: NextPage<Props>= ({ onChange, name, placeholder, isPassword}): JSX.Element => {
+const TextInputField: NextPage<Props> = ({
+  onChange,
+  name,
+  placeholder,
+  isPassword,
+  value,
+  white,
+}): JSX.Element => {
+  return (
+    <Input
+      height="70px"
+      width="100%"
+      borderRadius="10px"
+      padding="24px 32px"
+      margin="12px auto"
+      bg={white ? "#FFFFFF" : "#e7e7e7"}
+      _placeholder={{ color: "#7C7763" }}
+      border="none"
+      type={isPassword ? "password" : "text"}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+};
 
-    return(
-        <Input 
-            height='70px'
-            width='100%'
-            borderRadius='10px'
-            padding='24px 32px'
-            margin='12px auto'
-            bg='#e7e7e7'
-            _placeholder={{color:'#7C7763'}}
-            border='none'
-            type={ isPassword ? 'password' : 'text' } 
-            name={name} 
-            placeholder={placeholder} 
-            onChange={onChange}
-        />
-    )
-}
-
-export default TextInputField
+export default TextInputField;
---
+++ pages/dashboard/accept/index.tsx
@@ -0,0 +1,74 @@
+import { Box } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+import HeaderLeyout from "@/components/shared-components/layouts/header-layout";
+import ReviewHeader from "@/components/shared-components/review-section-header/review-header";
+import ClientInfo from "@/components/shared-components/client-info/client-info";
+import MessagesNotification from "@/components/dashboard/parent-dashboard/parent-header/header-element/messages-notification";
+import ReviewList from "@/components/shared-components/sections-list/review-list";
+import AcceptForm from "@/components/dashboard/retail-dashboard/accept-form";
+
+const myItems = [
+  {
+    id: "1",
+    name: "Transformer White/Blue",
+    image: "../../images/toy-example-image.png",
+    type: "Plastic Toy",
+    condition: "New",
+    deliver: true,
+    collection: true,
+    drop_locally: true,
+  },
+  {
+    id: "2",
+    name: "Transformer Optimus Prime",
+    image: "../../images/toy-example-image.png",
+    type: "Plastic Toy",
+    condition: "New",
+    deliver: false,
+    collection: true,
+    drop_locally: true,
+  },
+  {
+    id: "3",
+    name: "Transformer Optimus Prime",
+    image: "../../images/toy-example-image.png",
+    type: "Plastic Toy",
+    condition: "New",
+    deliver: true,
+    collection: false,
+    drop_locally: false,
+  },
+  {
+    id: "4",
+    name: "Transformer White/Blue",
+    image: "../../images/toy-example-image.png",
+    type: "Plastic Toy",
+    condition: "Used",
+    deliver: false,
+    collection: true,
+    drop_locally: true,
+  },
+];
+
+const MyInventory: NextPage = (): JSX.Element => {
+  const onRejectHandler = (type: string) => {
+    console.log(type);
+  };
+
+  const onAddHandler = (type: string) => {
+    console.log(type);
+  };
+
+  return (
+    <Box margin="50px 0">
+      <HeaderLeyout>
+        <ClientInfo message="My Inventory" />
+        <MessagesNotification />
+      </HeaderLeyout>
+      <AcceptForm />
+    </Box>
+  );
+};
+
+export default MyInventory;
